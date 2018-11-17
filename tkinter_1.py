aa2c4566f0c0d656e2a0221e13d900bbaa1d0e40
ola14 <32483970+ola14@users.noreply.github.com>
2018-11-17 17:46:38 +0200

    Add files via upload

---
+++ tkinter_1.py
@@ -0,0 +1,8 @@
+from tkinter import*
+#з ім'ям root,який буде батьківським
+root = Tk()
+#ініціалізація вікна 
+root.title('MyWindow')
+root.geometry('200x150+300+225')
+#запускаємо вікно
+root.mainloop()
