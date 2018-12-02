03bd6c45ee2761ad93776115f60658b46d48cb27
ola14 <32483970+ola14@users.noreply.github.com>
2018-12-02 21:12:05 +0200

    Add files via upload

---
+++ matplotlib_2.py
@@ -0,0 +1,35 @@
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = ['a', 'a', 'a', 'a', 'b', 'b', 'c', 'c', 'c', 'd', 'e', 'e', 'e', 'e', 'e']
+letter_counts = Counter(a)
+
+def plot_bar_from_counter(counter, ax=None):
+    """"
+    This function creates a bar plot from a counter.
+
+    :param counter: This is a counter object, a dictionary with the item as the key
+     and the frequency as the value
+    :param ax: an axis of matplotlib
+    :return: the axis wit the object in it
+    """
+
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+    frequencies = counter.values()
+    names = counter.keys()
+
+    x_coordinates = np.arange(len(counter))
+    ax.bar(x_coordinates, frequencies, align='center')
+
+    ax.xaxis.set_major_locator(plt.FixedLocator(x_coordinates))
+    ax.xaxis.set_major_formatter(plt.FixedFormatter(names))
+
+    return ax
+plt.savefig('histigrama.png', dpi = 200)
+
+plot_bar_from_counter(letter_counts)
+plt.show()
